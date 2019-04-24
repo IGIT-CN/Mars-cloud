34ebfe60a566553e2d35c6683e3aa49eaa89515a
yuye <631941527@qq.com>
2019-04-24 23:25:23 +0800

    :sparkles: 正式启动开发

---
+++ mars-cloud-start/src/main/java/com/mars/junit/MarsJunit.java
@@ -0,0 +1,22 @@
+package com.mars.junit;
+
+import com.mars.mybatis.init.InitJdbc;
+
+/**
+ * junit
+ */
+public abstract class MarsJunit {
+
+    /**
+     * 加载项目启动的必要数据
+     * @param packName
+     */
+    public void init(String packName){
+        MarsJunitStart.start(new InitJdbc(),packName,this);
+    }
+
+    /**
+     * 单测开始前
+     */
+    public abstract void before();
+}
---
+++ mars-cloud-start/src/main/java/com/mars/start/StartMars.java
@@ -0,0 +1,21 @@
+package com.mars.start;
+
+import com.mars.base.BaseStartMars;
+import com.mars.mybatis.init.InitJdbc;
+
+/**
+ * 启动Mars框架
+ * @author yuye
+ *
+ */
+public class StartMars {
+
+	/**
+	 * 启动Mars框架
+	 * @param clazz
+	 */
+	public static void start(Class<?> clazz) {
+		BaseStartMars.start(clazz,new InitJdbc());
+	}
+	
+}
