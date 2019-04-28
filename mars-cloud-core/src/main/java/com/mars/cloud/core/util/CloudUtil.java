101e98c23fd69d497e78d80ba434f20121a41455
yuye <631941527@qq.com>
2019-04-28 20:40:01 +0800

    :sparkles: 完成接口的注册与发现

--- mars-cloud-core/src/main/java/com/mars/cloud/core/util/CloudUtil.java
+++ mars-cloud-core/src/main/java/com/mars/cloud/core/util/CloudUtil.java
@@ -1,11 +1,7 @@
 package com.mars.cloud.core.util;
 
 import com.mars.core.util.ConfigUtil;
-
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.UnknownHostException;
-import java.util.Enumeration;
+import com.mars.core.util.MarsUtil;
 
 /**
  * 工具类
@@ -52,7 +48,7 @@ public class CloudUtil {
      */
     public static String getLocalIp() throws Exception {
         if(ip == null){
-            ip = getLocalHostLANAddress().getHostAddress();
+            ip = MarsUtil.getLocalHostLANAddress().getHostAddress();
         }
         return ip;
     }
@@ -88,48 +84,6 @@ public class CloudUtil {
         }
         if(localHost == null){
             localHost = protocol+"://"+ip+":"+port;
-        }
-    }
-
-    /**
-     * 获取本服务的IP
-     * @return
-     * @throws UnknownHostException
-     */
-    private static InetAddress getLocalHostLANAddress() throws UnknownHostException {
-        try {
-            InetAddress candidateAddress = null;
-            // 遍历所有的网络接口
-            for (Enumeration ifaces = NetworkInterface.getNetworkInterfaces(); ifaces.hasMoreElements();) {
-                NetworkInterface iface = (NetworkInterface) ifaces.nextElement();
-                // 在所有的接口下再遍历IP
-                for (Enumeration inetAddrs = iface.getInetAddresses(); inetAddrs.hasMoreElements();) {
-                    InetAddress inetAddr = (InetAddress) inetAddrs.nextElement();
-                    if (!inetAddr.isLoopbackAddress()) {// 排除loopback类型地址
-                        if (inetAddr.isSiteLocalAddress()) {
-                            // 如果是site-local地址，就是它了
-                            return inetAddr;
-                        } else if (candidateAddress == null) {
-                            // site-local类型的地址未被发现，先记录候选地址
-                            candidateAddress = inetAddr;
-                        }
-                    }
-                }
-            }
-            if (candidateAddress != null) {
-                return candidateAddress;
-            }
-            // 如果没有发现 non-loopback地址.只能用最次选的方案
-            InetAddress jdkSuppliedAddress = InetAddress.getLocalHost();
-            if (jdkSuppliedAddress == null) {
-                throw new UnknownHostException("The JDK InetAddress.getLocalHost() method unexpectedly returned null.");
-            }
-            return jdkSuppliedAddress;
-        } catch (Exception e) {
-            UnknownHostException unknownHostException = new UnknownHostException(
-                    "Failed to determine LAN address: " + e);
-            unknownHostException.initCause(e);
-            throw unknownHostException;
         }
     }
 }
