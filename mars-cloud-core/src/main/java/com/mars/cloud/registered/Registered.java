a2f6931de855cf715b83370cf40e838d912dba92
yuye <631941527@qq.com>
2019-11-23 20:03:30 +0800

    update version

--- mars-cloud-core/src/main/java/com/mars/cloud/registered/Registered.java
+++ mars-cloud-core/src/main/java/com/mars/cloud/registered/Registered.java
@@ -48,22 +48,26 @@ public class Registered {
 
             /* 注册接口 */
             for (String methodName : maps.keySet()) {
-                Boolean isNotRest = checkIsRest(maps,methodName);
-                /* 返回true代表这个接口不是rest接口，而是一个普通的http接口，所以不需要注册 */
-                if(isNotRest){
-                    checkRequestMethod(maps,methodName);
 
-                    String node = CloudConstant.API_SERVER_NODE
-                            .replace("{serverName}", serverName)
-                            .replace("{method}", methodName)
-                            .replace("{ip}", ip)
-                            .replace("{port}", port);
-
-                    /* 将本服务的接口已写入zookeeper */
-                    ZkHelper.createNodes(node, CloudUtil.getLocalHost() + "/" + methodName);
-
-                    marsLogger.info("接口[" + CloudUtil.getLocalHost() + "/" + methodName + "]注册成功");
+                /* 返回null代表这个接口不是rest接口，而是一个普通的http接口，所以不需要注册 */
+                MarsMappingModel marsMappingModel = checkIsRest(maps,methodName);
+                if(marsMappingModel == null){
+                    continue;
                 }
+
+                /* 校验请求方式是否为POST */
+                checkRequestMethod(marsMappingModel);
+
+                /* 将本服务的接口写入zookeeper */
+                String node = CloudConstant.API_SERVER_NODE
+                        .replace("{serverName}", serverName)
+                        .replace("{method}", methodName)
+                        .replace("{ip}", ip)
+                        .replace("{port}", port);
+
+                ZkHelper.createNodes(node, CloudUtil.getLocalHost() + "/" + methodName);
+
+                marsLogger.info("接口[" + CloudUtil.getLocalHost() + "/" + methodName + "]注册成功");
             }
         } catch (Exception e) {
             throw new Exception("注册与发布接口失败", e);
@@ -86,12 +90,10 @@ public class Registered {
 
     /**
      * 校验cloud接口是否为post
-     * @param maps
-     * @param methodName
+     * @param marsMappingModel
      * @throws Exception
      */
-    private void checkRequestMethod(Map<String, MarsMappingModel> maps,String methodName) throws Exception {
-        MarsMappingModel marsMappingModel = maps.get(methodName);
+    private void checkRequestMethod(MarsMappingModel marsMappingModel) throws Exception {
         if(!marsMappingModel.getReqMethod().equals(ReqMethod.POST)){
             throw new Exception("MarsCloud对外提供的接口必须是POST方式");
         }
@@ -103,13 +105,13 @@ public class Registered {
      * @param methodName
      * @return true 不是rest，false 是rest
      */
-    private Boolean checkIsRest(Map<String, MarsMappingModel> maps,String methodName){
+    private MarsMappingModel checkIsRest(Map<String, MarsMappingModel> maps,String methodName){
         MarsMappingModel marsMappingModel = maps.get(methodName);
         Class<?> controllerCls = marsMappingModel.getCls();
         NotRest notRest = controllerCls.getAnnotation(NotRest.class);
         if(notRest == null){
-           return true;
+           return marsMappingModel;
         }
-        return false;
+        return null;
     }
 }
